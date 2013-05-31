eadcec30c1a2ca8c4651f8c4a35fa9f7130bddfa
dblock <dblock@dblock.org>
2013-05-31 12:41:16 -0400

    Added tests and Travis configuration.

---
+++ spec/memcachier_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe "Memcachier" do
+  let(:memcachier) { File.join(File.dirname(__FILE__), '..', 'lib', 'memcachier.rb') }
+  [ "MEMCACHE_SERVERS", "MEMCACHE_USERNAME", "MEMCACHE_PASSWORD" ].each do |var|
+    context var do
+      let(:memcachier_var) { var.gsub "MEMCACHE", "MEMCACHIER" }
+      before :each do
+        ENV[var] = nil
+        ENV[memcachier_var] = nil
+      end
+      it "not set by default" do
+        load memcachier
+        ENV[var].should be_nil
+      end
+      it "copied from memcachier" do
+        ENV[memcachier_var] = "set"
+        load memcachier
+        ENV[var].should == "set"
+      end
+      it "overwritten by memcachier" do
+        ENV[var] = "old"
+        ENV[memcachier_var] = "new"
+        load memcachier
+        ENV[var].should == "new"
+      end
+      it "not removed by memcachier" do
+        ENV[var] = "old"
+        load memcachier
+        ENV[var].should == "old"
+      end
+    end
+  end
+end
